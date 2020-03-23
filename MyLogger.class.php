38c48ce63ee2156d29668661b05171dec78f767f
Lars <larstwolters@gmail.com>
2020-03-23 11:24:21 +0100

    Uitwerking van de exercise

--- MyLogger.class.php
+++ MyLogger.class.php
@@ -20,23 +20,31 @@ class MyLogger
         }
     }
 
+    private function logWithTime($message)
+    {
+        return '[' . date('Y-m-d H:i', time()) . '] ' . $message;
+    }
+
     public function warning($message)
     {
-        print_r('Warning:' . $message);
+        print_r($this->logWithTime('Warning: ' . $message));
     }
 
     public function error($message)
     {
-        print_r('Error:' . $message);
+        print_r($this->logWithTime('Error: ' . $message));
     }
 
     public function info($message)
     {
-        print_r('Info:' . $message);
+        print_r($this->logWithTime('Info: ' . $message));
     }
 
     public function debug($message)
     {
-        print_r('Debug:' . $message);
+        print_r($this->logWithTime('Debug: ' . $message));
     }
 }
+
+$logger = new MyLogger();
+$logger->log('warning', 'Bla Bla');
